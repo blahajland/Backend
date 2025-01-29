0039cba4a68237428343226443027092acaa5b7e
zickles <zicklesistaken@gmail.com>
2025-01-29 12:44:21 -0500

    feat: middleware

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -9,7 +10,16 @@ import (
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	r.Use(Middleware)
+
 	http.ListenAndServe(":8080", r)
+}
+
+func Middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
