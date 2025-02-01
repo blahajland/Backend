9a50eda5da952dd296ecc89d774d0a0e9ccd449d
ErikLavr <106452123+ErikLavr@users.noreply.github.com>
2025-02-01 20:05:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", HomeHandler)
+	r.HandleFunc("/", HomeHandler)                         // POST
+	r.HandleFunc("/api/sites/create", SiteCreationHandler) // POST
+	r.HandleFunc("/api/sites/delete", SiteDeletionHandler) // POST
+	r.HandleFunc("/api/sites/update", SiteListHandler)     // POST
+	r.HandleFunc("/api/sites/list", SiteCreationHandler)   // POST
+	r.HandleFunc("/api/sites/login", SiteLoginHandler)     // POST
 	r.Use(Middleware)
 
 	http.ListenAndServe(":8080", r)
@@ -22,6 +27,21 @@ func Middleware(next http.Handler) http.Handler {
 	})
 }
 
+func SiteCreationHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+func SiteDeletionHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+func SiteUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+func SiteListHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
+func SiteLoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
 }
